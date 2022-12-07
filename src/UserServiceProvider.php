5972d9cdbf9275eac2ca9d326c46c3caf8a8e16a
Sorethea OK <sorethea@gmail.com>
2022-12-07 09:19:48 +0700

    update composer.json

--- src/UserServiceProvider.php
+++ src/UserServiceProvider.php
@@ -10,6 +10,6 @@ class UserServiceProvider extends PluginServiceProvider
     public function configurePackage(Package $package): void
     {
         $package->name("dd-user")
-            ->hasMigration("");
+            ->hasMigration("alter_users_table");
     }
 }
