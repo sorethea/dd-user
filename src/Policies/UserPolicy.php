59c5a070fe1781169c4dd4a164c16adc82b0745e
Sorethea OK <sorethea@gmail.com>
2022-12-08 14:24:23 +0700

    UserResource.php

--- src/Policies/UserPolicy.php
+++ src/Policies/UserPolicy.php
@@ -11,30 +11,36 @@ class UserPolicy
 
     public function viewAny(User $user): bool
     {
-
+        return $user->can("users.viewAny");
     }
 
     public function view(User $user, User $model): bool
     {
+        return $user->can("users.view");
     }
 
     public function create(User $user): bool
     {
+        return $user->can("users.create");
     }
 
     public function update(User $user, User $model): bool
     {
+        return $user->can("users.update");
     }
 
     public function delete(User $user, User $model): bool
     {
+        return $user->can("users.delete");
     }
 
     public function restore(User $user, User $model): bool
     {
+        return $user->can("users.restore");
     }
 
     public function forceDelete(User $user, User $model): bool
     {
+        return $user->can("users.forceDelete");
     }
 }
