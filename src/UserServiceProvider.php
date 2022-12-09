be554931b57a0b358101292d8c500415daa6a049
Sorethea OK <sorethea@gmail.com>
2022-12-09 15:57:03 +0700

    AuthServiceProvider.php

--- src/UserServiceProvider.php
+++ src/UserServiceProvider.php
@@ -3,13 +3,15 @@
 namespace Sorethea\DdUser;
 
 use Illuminate\Support\ServiceProvider;
+use Sorethea\DdUser\FilamentServiceProvider;
+use Sorethea\DdUser\AuthServiceProvider;
 
 class UserServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->register(Sorethea\DdUser\FilamentServiceProvider::class);
-        $this->app->register(Sorethea\DdUser\AuthServiceProvider::class);
+        $this->app->register(FilamentServiceProvider::class);
+        $this->app->register(AuthServiceProvider::class);
     }
 
     public function boot()
